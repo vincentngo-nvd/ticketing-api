02f8bcbce88b0ca39a4d1e958fbb8f75af1c8a3e
VanDuongNgo <ngovanduong.dev@gmail.com>
2026-05-28 12:06:06 +0700

    feat: add welcome endpoint for api v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    message: "Welcome to the Event Ticketing API",
+    version: "v1.0.0",
+    documentation: "https://github.com/ngovanduong-dev/ticketing-api#readme"
+  });
+});
+
 router.use('/auth', require('./auth.routes'));
 router.use('/events', require('./event.routes'));
 router.use('/bookings', require('./booking.routes'));
